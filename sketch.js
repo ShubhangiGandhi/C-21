4abfd80cc46d73bbca82ee6dc4e7438ad637863f
ShubhangiGandhi <64540123+ShubhangiGandhi@users.noreply.github.com>
2020-05-08 17:56:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,32 @@
+var fixedRect, movingRect;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "pink";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "pink";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0,0,0);
+  BounceOff(movingRect,fixedRect);  
   drawSprites();
+}
+function BounceOff(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2) {
+  object1.velocityX = object1.velocityX * (-1);
+  object2.velocityX = object2.velocityX * (-1);
+}
+if (object1.y - object2.y < object2.height/2 + object1.height/2
+  && object2.y - object1.y < object2.height/2 + object1.height/2){
+  object1.velocityY = object1.velocityY * (-1);
+  object2.velocityY = object2.velocityY * (-1);
+} 
 }
